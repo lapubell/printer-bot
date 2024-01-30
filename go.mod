36650478b54e528cd2342a561f38548f060e0980
Kurtis Holsapple <kurtis@openfunctioncomputers.com>
2024-01-30 14:29:58 -0800

    hardcoded stuff to process images

--- go.mod
+++ go.mod
@@ -3,8 +3,12 @@ module github.com/lapubell/printer-bot
 go 1.21.3
 
 require (
+	github.com/gabriel-vasile/mimetype v1.4.3
 	github.com/joho/godotenv v1.5.1
 	github.com/sirupsen/logrus v1.9.3
 )
 
-require golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
+require (
+	golang.org/x/net v0.17.0 // indirect
+	golang.org/x/sys v0.13.0 // indirect
+)
--- server/handleUpload.go
+++ server/handleUpload.go
@@ -8,7 +8,10 @@ import (
 	"os"
 	"os/exec"
 	"strconv"
+	"strings"
 	"time"
+
+	"github.com/gabriel-vasile/mimetype"
 )
 
 func (s *server) handleUpload() http.HandlerFunc {
@@ -23,7 +26,7 @@ func (s *server) handleUpload() http.HandlerFunc {
 		}
 		defer file.Close()
 
-		fileName := strconv.Itoa(int(time.Now().Unix())) + handler.Filename
+		fileName := "/tmp/" + strconv.Itoa(int(time.Now().Unix())) + "." + handler.Filename
 
 		localFile, err := os.Create(fileName)
 		if err != nil {
@@ -37,26 +40,115 @@ func (s *server) handleUpload() http.HandlerFunc {
 
 		go func() {
 			time.Sleep(time.Second * 1)
+			s.logger.Info("gonna try and print " + fileName)
 
-			stdout, err := exec.Command("/usr/bin/lpr", "-P", "EPSON_XP_7100_Series_USB", fileName).Output()
+			mimeType, err := mimetype.DetectFile(fileName)
 			if err != nil {
-				s.logger.Error(err)
+				s.logger.Error("bummer! " + err.Error())
 				return
 			}
-			s.logger.Info(stdout)
+
+			if mimeType.Is("image/jpeg") {
+				s.logger.Info("Now printing an image!")
+
+				// check if needs rotation
+				stdout, err := exec.Command("/usr/bin/identify", "-verbose", fileName).Output()
+				if err != nil {
+					s.logger.Error("bad info: " + err.Error())
+					return
+				}
+				lines := strings.Split(string(stdout), "\n")
+				geometryLine := ""
+
+				for _, line := range lines {
+					if len(line) < 13 {
+						continue
+					}
+
+					if line[0:12] == "  Geometry: " {
+						geometryLine = strings.Replace(line, "  Geometry: ", "", 1)
+						geometryLine = strings.Replace(geometryLine, "+0+0", "", 1)
+						break
+					}
+				}
+
+				dimentions := strings.Split(geometryLine, "x")
+				if len(dimentions) != 2 {
+					s.logger.Error("bad dimentions!", dimentions, geometryLine)
+					return
+				}
+				s.logger.Info("Dimentions: ", dimentions)
+
+				width, err := strconv.Atoi(dimentions[0])
+				if err != nil {
+					s.logger.Error("width ", err.Error())
+					return
+				}
+				height, err := strconv.Atoi(dimentions[1])
+				if err != nil {
+					s.logger.Error("height ", err.Error())
+					return
+				}
+
+				if width > height {
+					s.logger.Info("wider than tall, ROTATE!")
+					exec.Command("/usr/bin/convert", fileName, "-rotate", "-90", fileName).Output()
+				}
+
+				// force the image to 4x6
+				resizedFilename := strings.ReplaceAll(fileName, "/tmp/", "/tmp/resized-")
+				croppedFilename := strings.ReplaceAll(fileName, "/tmp/", "/tmp/cropped-")
+				printerFilename := strings.ReplaceAll(fileName, "/tmp/", "/tmp/print-")
+				printerFilename = strings.ReplaceAll(printerFilename, ".jpg", ".pdf")
+
+				// resize file and delete og image
+				s.logger.Info("resizing...", fileName, resizedFilename)
+				exec.Command("/usr/bin/convert", fileName, "-resize", "1920x2880^", resizedFilename).Output()
+
+				s.logger.Info("cropping...", resizedFilename, croppedFilename)
+				// crop file and delete resized image
+				exec.Command("/usr/bin/convert", resizedFilename, "-gravity", "center", "-crop", "1920x2880+0+0", "+repage", croppedFilename).Output()
+
+				// convert to pdf and delete cropped image
+				s.logger.Info("converting...", printerFilename, croppedFilename)
+				exec.Command("/usr/local/bin/pdfcpu", "import", printerFilename, croppedFilename).Output()
+
+				// print this thing!
+				s.logger.Info("printing!" + printerFilename)
+				stdout, err = exec.Command("/usr/bin/lpr", "-P", "EPSON_XP_7100_Series_USB", "-o", "InputSlot=Photo", "-o", "PageSize=4x6.Borderless", printerFilename).Output()
+				if err != nil {
+					s.logger.Error(err)
+					return
+				}
+				s.logger.Info(stdout)
+				return
+			}
+
+			if mimeType.Is("application/pdf") {
+				s.logger.Info("Now printing a PDF!")
+				stdout, err := exec.Command("/usr/bin/lpr", "-P", "EPSON_XP_7100_Series_USB", fileName).Output()
+				if err != nil {
+					s.logger.Error(err)
+					return
+				}
+				s.logger.Info(stdout)
+				return
+			}
+
+			s.logger.Error("not printing anything, invalid mime type :(", mimeType)
 		}()
 
-		go func() {
-			time.Sleep(time.Second * 30)
-			os.Remove(fileName)
-		}()
+		// go func() {
+		// 	time.Sleep(time.Second * 30)
+		// 	os.Remove(fileName)
+		// }()
 
 		output := map[string]string{
 			"message": "yay!",
 		}
 
-		bytes, _ := json.Marshal(output)
+		outputBytes, _ := json.Marshal(output)
 
-		w.Write(bytes)
+		w.Write(outputBytes)
 	}
 }
